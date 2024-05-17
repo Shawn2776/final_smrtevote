aa4056ac93d10a5b3e99bc4602bad39c0e92689a
sdharrin <138178989+sdharrin@users.noreply.github.com>
2024-05-17 13:33:02 -0700

    reset pw in suspense wrapper

--- components/auth/new-password-form.jsx
+++ components/auth/new-password-form.jsx
@@ -1,9 +1,10 @@
 "use client";
 
 import * as z from "zod";
-
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useState, useTransition, Suspense } from "react";
+import { useSearchParams } from "next/navigation";
 
 import { NewPasswordSchema, RegisterSchema } from "@/schemas";
 import {
@@ -14,21 +15,15 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-
 import CardWrapper from "@/components/auth/card-wrapper";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { FormError } from "../form-error";
 import { FormSuccess } from "../form-success";
 import { register } from "@/actions/register";
-import { useState, useTransition } from "react";
-import { useSearchParams } from "next/navigation";
 import { newPassword } from "@/actions/new-password";
 
 const NewPasswordForm = () => {
-  const searchParams = useSearchParams();
-  const token = searchParams.get("token");
-
   const [error, setError] = useState("");
   const [success, setSuccess] = useState("");
 
@@ -52,6 +47,24 @@ const NewPasswordForm = () => {
       });
     });
   };
+
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SearchParamsForm
+        form={form}
+        onSubmit={onSubmit}
+        error={error}
+        success={success}
+        isPending={isPending}
+      />
+    </Suspense>
+  );
+};
+
+const SearchParamsForm = ({ form, onSubmit, error, success, isPending }) => {
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+
   return (
     <CardWrapper
       headerLabel={"Enter a New Password"}
