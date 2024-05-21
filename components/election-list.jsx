479c4261d47b250bdf3c6dcb62aba1c247b8605a
sdharrin <138178989+sdharrin@users.noreply.github.com>
2024-05-21 15:49:44 -0700

    mobile view changes

--- components/election-list.jsx
+++ components/election-list.jsx
@@ -13,58 +13,73 @@ const ElectionsList = ({ elections }) => {
   return (
     <>
       <div className="w-full h-full flex flex-col lg:hidden">
-        <div className="bg-white w-[98%] mx-auto px-2 py-1 rounded-md">
-          <ul>
-            <li className="flex justify-between items-center border-b-2 border-black mb-4">
-              <div className="space-y-0">
-                <div className="text-xl m-0 p-0">Election Name</div>
-                <div className="text-xs">election description</div>
-              </div>
-              <div className="pr-4">Actions</div>
-            </li>
-
-            {elections.map((election, index) => (
-              <li
-                key={election.id}
-                className={`flex justify-between items-center ${
+        <ul className="bg-white">
+          <li className="my-4">
+            <div className="flex w-full font-extrabold pl-4">
+              <p className="w-4/6">Name</p>
+              <p className="w-1/6">Status</p>
+              <p className="w-1/6">Actions</p>
+            </div>
+          </li>
+          {elections.map((election, index) => (
+            <li key={election.id}>
+              <div
+                className={`w-full flex justify-center pt-2 pb-2 pl-2 text-sm gap-4 items-center ${
                   index % 2 === 0 ? "bg-gray-300" : ""
                 }`}
               >
-                <div
-                  className={`flex flex-col text-sm py-2 ${
-                    index % 2 === 0 ? "bg-gray-300" : ""
-                  }`}
-                >
-                  <div className="pl-2">
-                    <p>{election.name}</p>
-                    <p className="text-xs text-gray-800">
-                      {election.description}
-                    </p>
-                  </div>
+                <div className="w-4/6 flex flex-col">
+                  <p>{election.name}</p>
+                  <span className="text-xs text-gray-700">
+                    {election.description}
+                  </span>
                 </div>
-                <div className="pr-4">
-                  <DropdownMenu>
-                    <DropdownMenuTrigger>
-                      <BiDotsHorizontalRounded className="w-6 h-6" />
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent className="w-40" align="end">
-                      <Link
-                        href={`/elections/${election.id}`}
-                        variant="ghost"
-                        className="w-full"
-                      >
+
+                <div className="w-1/6">
+                  <p className="w-full">
+                    <span
+                      className={`text-xs flex justify-start px-2 py-1 border shadow-md shadow-black border-black ${
+                        election.status === "PENDING"
+                          ? "bg-orange-400 text-black"
+                          : election.status === "OPEN"
+                          ? "bg-green-400 text-black"
+                          : "bg-red-400 text-black"
+                      }`}
+                    >
+                      {election.status}
+                    </span>
+                  </p>
+                </div>
+
+                <div className="w-1/6 flex justify-center">
+                  <p>
+                    <DropdownMenu>
+                      <DropdownMenuTrigger>
+                        <BiDotsHorizontalRounded className="w-6 h-6" />
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent className="w-32" align="end">
+                        <Link
+                          href={`/elections/${election.id}`}
+                          variant="ghost"
+                          className="w-full"
+                        >
+                          <DropdownMenuItem className="flex items-center gap-4">
+                            <FaEye className="w-4 h-4 mr-2" />
+                            View
+                          </DropdownMenuItem>
+                        </Link>
                         <DropdownMenuItem className="flex items-center gap-4">
-                          <FaEye className="w-4 h-4 mr-2" />
-                          View
+                          <DeleteIcon className="w-4 h-4 mr-2" />
+                          Delete
                         </DropdownMenuItem>
-                      </Link>
-                    </DropdownMenuContent>
-                  </DropdownMenu>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </p>
                 </div>
-              </li>
-            ))}
-          </ul>
-        </div>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
       <div className="hidden lg:flex w-full h-full">
         <div className="p-4 ml-10 mr-10 bg-white rounded-md">
@@ -77,20 +92,33 @@ const ElectionsList = ({ elections }) => {
                 <p>Status</p>
                 <p>Type</p>
                 <p>Candidates</p>
+                <p>Actions</p>
               </div>
             </li>
             {elections.map((election, index) => (
               <li key={election.id}>
                 <div
-                  className={`grid grid-cols-7 pt-2 pb-2 pl-2 text-sm gap-4 ${
+                  className={`grid grid-cols-7 pt-2 pb-2 pl-2 text-sm gap-4 items-center ${
                     index % 2 === 0 ? "bg-gray-300" : ""
                   }`}
                 >
                   <p>{election.name}</p>
                   <p>{election.description}</p>
                   <p>{election.electionDate}</p>
-                  <p>{election.status}</p>
-                  <p>{election.electionType}</p>
+                  <p>
+                    <span
+                      className={`px-2 py-1 border shadow-md shadow-black border-black ${
+                        election.status === "PENDING"
+                          ? "bg-orange-400 text-black"
+                          : election.status === "OPEN"
+                          ? "bg-green-400 text-black"
+                          : "bg-red-400 text-black"
+                      }`}
+                    >
+                      {election.status}
+                    </span>
+                  </p>
+                  <p className="capitalize">{election.electionType}</p>
                   <p>{election.candidates}</p>
                   <p>
                     <Link
