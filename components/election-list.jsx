0fcf0d072c320ebc9dd37d32ebbd7a4beb50124f
Shawn Harrington <shawn@2776.ltd>
2024-05-22 22:30:07 -0700

    dashboard work vusals

--- components/election-list.jsx
+++ components/election-list.jsx
@@ -5,14 +5,18 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
-import { DeleteIcon } from "lucide-react";
+import { DeleteIcon, Edit2Icon, Trash2Icon } from "lucide-react";
+import { TbListDetails } from "react-icons/tb";
+
 import { BiDotsHorizontalRounded } from "react-icons/bi";
 import { FaEye } from "react-icons/fa";
 import { Button } from "./ui/button";
+import { DotsHorizontalIcon } from "@radix-ui/react-icons";
 
 const ElectionsList = ({ elections }) => {
   return (
     <>
+      {/* MOBILE */}
       <div className="flex flex-col w-full h-full lg:hidden">
         <ul className="bg-white">
           <li className="my-4">
@@ -82,33 +86,37 @@ const ElectionsList = ({ elections }) => {
           ))}
         </ul>
       </div>
-      <div className="hidden w-full h-full lg:flex">
-        <div className="p-4 ml-10 mr-10 bg-white rounded-md">
+      {/* DESKTOP */}
+      <div className="hidden w-full h-full lg:flex lg:flex-col">
+        <hr className="w-full border-t border-gray-300" />
+        <div className="w-full p-4 pl-10 pr-10 bg-white">
           <ul>
             <li>
-              <div className="grid grid-cols-7 pl-4 mb-4 font-extrabold">
-                <p>Name</p>
-                <p>Description</p>
-                <p>Date</p>
-                <p>Status</p>
-                <p>Type</p>
-                <p>Candidates</p>
-                <p>Actions</p>
+              <div className="grid grid-cols-10 pl-4 mb-4 font-extrabold">
+                <p className="col-span-2">Name</p>
+                <p className="col-span-2">Description</p>
+                <p className="col-span-1 text-center">Date</p>
+                <p className="col-span-1 text-center">Status</p>
+                <p className="col-span-1 text-center">Type</p>
+                <p className="col-span-2 text-center">Candidates</p>
+                <p className="col-span-1 text-center">Actions</p>
               </div>
             </li>
             {elections.map((election, index) => (
-              <li key={election.id}>
-                <div
-                  className={`grid grid-cols-7 pt-2 pb-2 pl-2 text-sm gap-4 items-center ${
+              <li key={election.id} className="">
+                <Link
+                  href={`/elections/${election.id}/overview`}
+                  className={`grid grid-cols-10 pt-2 pb-2 pl-2 text-sm gap-6 items-center hover:bg-slate-900 hover:text-white hover:cursor-pointer ${
                     index % 2 === 0 ? "bg-gray-300" : ""
                   }`}
                 >
-                  <p>{election.name}</p>
-                  <p>{election.description}</p>
-                  <p>{election.electionDate}</p>
-                  <p>
+                  {" "}
+                  <p className="col-span-2 ">{election.name}</p>
+                  <p className="col-span-2 ">{election.description}</p>
+                  <p className="col-span-1 ">{election.electionDate}</p>
+                  <p className="w-full col-span-1 mx-auto text-center ">
                     <span
-                      className={`px-2 py-1 border shadow-md shadow-black border-black ${
+                      className={`w-[100%] px-2 py-1 col-span-1 border shadow-md shadow-black border-black ${
                         election.status === "PENDING"
                           ? "bg-orange-400 text-black"
                           : election.status === "OPEN"
@@ -119,20 +127,54 @@ const ElectionsList = ({ elections }) => {
                       {election.status}
                     </span>
                   </p>
-                  <p className="capitalize">{election.electionType}</p>
-                  <p>{election.candidates}</p>
-                  <p>
-                    <Button asChild variant="outline">
-                      <Link
-                        href={`/elections/${election.id}/overview`}
-                        variant="outline"
-                        className="w-full"
-                      >
-                        View
-                      </Link>
-                    </Button>
+                  <p className="col-span-1 text-center capitalize">
+                    {election.electionType}
                   </p>
-                </div>
+                  <p className="col-span-2 ">{election.candidates}</p>
+                  <Button variant="link" className="hover:text-white">
+                    <DropdownMenu>
+                      <DropdownMenuTrigger>
+                        <DotsHorizontalIcon className="w-6 h-6" />
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent
+                        className="w-40 hover:cursor-pointer"
+                        align="end"
+                      >
+                        <Link
+                          href="/new-election"
+                          variant="ghost"
+                          className="w-full"
+                        >
+                          <DropdownMenuItem className="flex items-center gap-4 hover:cursor-pointer">
+                            <TbListDetails className="w-4 h-4 mr-2 " />
+                            Details
+                          </DropdownMenuItem>
+                        </Link>
+                        <Link
+                          href="/profile"
+                          variant="ghost"
+                          className="w-full"
+                        >
+                          <DropdownMenuItem className="flex items-center gap-4 hover:cursor-pointer">
+                            <Edit2Icon className="w-4 h-4 mr-2 " />
+                            Edit
+                          </DropdownMenuItem>
+                        </Link>
+                        <Link
+                          href="/settings"
+                          variant="ghost"
+                          className="w-full"
+                        >
+                          <DropdownMenuItem className="flex items-center gap-4 hover:cursor-pointer">
+                            <Trash2Icon className="w-4 h-4 mr-2 " />
+                            Delete
+                          </DropdownMenuItem>
+                        </Link>
+                        <hr className="my-2" />
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </Button>
+                </Link>
               </li>
             ))}
           </ul>
