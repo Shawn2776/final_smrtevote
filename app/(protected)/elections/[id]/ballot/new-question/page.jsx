63dbd13e3ca37ce1160aba0dc663c6d38e6e7b0e
Shawn Harrington <shawn@2776.ltd>
2024-05-27 13:14:35 -0700

    working on candidates

--- app/(protected)/elections/[id]/ballot/new-question/page.jsx
+++ app/(protected)/elections/[id]/ballot/new-question/page.jsx
@@ -1,11 +1,10 @@
-import NewQuestionTestForm from "@/actions/test";
 import NewQuestionForm from "@/components/question-form";
 import React from "react";
 
 const BallotPage = () => {
   return (
     <div className="flex items-center justify-center w-full h-full">
-      <NewQuestionTestForm />
+      <NewQuestionForm />
     </div>
   );
 };
